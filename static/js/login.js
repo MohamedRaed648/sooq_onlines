06df8f30619140bb537c15319d0a2f60f3ef7367
unknown <ajac4390@gmail.com>
2021-07-28 23:51:14 +0300

    nothing

---
+++ static/js/login.js
@@ -0,0 +1,4 @@
+let userName=document.getElementById("id_username");
+let passWord=document.getElementById("id_password");
+userName.style.border="1px solid red";
+passWord.style.border="1px solid red";
